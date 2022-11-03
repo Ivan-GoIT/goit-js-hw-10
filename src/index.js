818f492317dae7f1d36be60294d58ec92fe8d1d2
IvanKoshel98 <GitHubPassvord142>
2022-11-03 16:19:28 +0200

    onKeyDown begining

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import './sass/styles.scss';
+import { refs } from './js/refs';
 
 const DEBOUNCE_DELAY = 300;
+
+const { searchBoxEl, counrtyListEl, counrtyInfoEl } = refs;
+
+const onKeyDown = ()=>{console.log('!!!');}
+
+searchBoxEl.addEventListener('keyDown',onKeyDown)
---
+++ src/js/refs.js
@@ -0,0 +1,5 @@
+export let refs ={
+  searchBoxEl: document.querySelector('#search-box'),
+  counrtyListEl: document.querySelector('.country-list'),
+  counrtyInfoEl: document.querySelector('.country-info'),
+};
